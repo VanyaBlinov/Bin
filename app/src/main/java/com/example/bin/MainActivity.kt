44556032c8e22ed7c62d14cd412763eaa59b7148
Vanya Blinov <iv-blin@yandex.ru>
2023-02-02 07:50:40 +0700

    add binding, put JSONObject

--- app/src/main/java/com/example/bin/MainActivity.kt
+++ app/src/main/java/com/example/bin/MainActivity.kt
@@ -2,12 +2,42 @@ package com.example.bin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.bin.databinding.ActivityMainBinding
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bGet.setOnClickListener {
+            val number = binding.edNumber.text.toString()
+            getResult(number)
+        }
+    }
+    private fun getResult(number: String) {
+        val url = "https://lookup.binlist.net/$number"
+        val queue = Volley.newRequestQueue(this)
+        val stringRequest = StringRequest(Request.Method.GET,
+            url,
+            {
+              response->
+                val obj = JSONObject(response)
+                val scheme = obj.getJSONObject("number")
+                Log.d("MyLog", "Response: ${scheme.getString("length")}")
+            },
+            {
+                Log.d("MyLog", "Volley error: $it")
+            }
+            )
+        queue.add(stringRequest)
     }
 }
+
+
+//45717360
